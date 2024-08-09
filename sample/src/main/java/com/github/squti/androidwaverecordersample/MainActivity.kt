ecce0118ae124077274527dddcc9615e7aad9dfb
squti <sqquti@gmail.com>
2024-08-09 17:27:15 +0330

    Fix external storage file saving compatibility for Android 9 and earlier versions in sample

--- sample/src/main/java/com/github/squti/androidwaverecordersample/MainActivity.kt
+++ sample/src/main/java/com/github/squti/androidwaverecordersample/MainActivity.kt
@@ -25,6 +25,7 @@
 package com.github.squti.androidwaverecordersample
 
 import android.Manifest
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -32,6 +33,7 @@ import android.media.AudioFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
@@ -44,12 +46,13 @@ import androidx.core.content.ContextCompat
 import com.github.squti.androidwaverecorder.RecorderState
 import com.github.squti.androidwaverecorder.WaveRecorder
 import com.github.squti.androidwaverecordersample.databinding.ActivityMainBinding
-import java.io.IOException
+import java.io.File
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private val PERMISSIONS_REQUEST_RECORD_AUDIO = 77
+    private val PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 88
 
     private lateinit var waveRecorder: WaveRecorder
     private lateinit var filePath: String
@@ -65,39 +68,70 @@ class MainActivity : AppCompatActivity() {
         initRecorder(isSaveToExternalStorage = false)
 
         binding.saveToExternalStorageSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                initRecorder(isSaveToExternalStorage = true)
-            } else {
+            if (!isChecked) {
+                resetSwitches()
                 initRecorder(isSaveToExternalStorage = false)
+                return@setOnCheckedChangeListener
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                resetSwitches()
+                initRecorder(isSaveToExternalStorage = true)
+                return@setOnCheckedChangeListener
+            }
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                resetSwitches()
+                initRecorder(isSaveToExternalStorage = true)
+                return@setOnCheckedChangeListener
+            }
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
+                showPermissionSettingsDialog()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+                )
             }
         }
 
         binding.startStopRecordingButton.setOnClickListener {
-
-            if (!isRecording) {
-                if (ContextCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.RECORD_AUDIO
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(
-                            this,
-                            Manifest.permission.RECORD_AUDIO
-                        )
-                    ) {
-                        ActivityCompat.requestPermissions(
-                            this,
-                            arrayOf(Manifest.permission.RECORD_AUDIO),
-                            PERMISSIONS_REQUEST_RECORD_AUDIO
-                        )
-                    } else {
-                        showPermissionSettingsDialog()
-                    }
-                } else {
-                    waveRecorder.startRecording()
-                }
-            } else {
+            if (isRecording) {
                 waveRecorder.stopRecording()
+                return@setOnClickListener
+            }
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                waveRecorder.startRecording()
+                return@setOnClickListener
+            }
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                )
+            ) {
+                showPermissionSettingsDialog()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    PERMISSIONS_REQUEST_RECORD_AUDIO
+                )
             }
         }
 
@@ -109,32 +143,45 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.showAmplitudeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.amplitudeTextView.text = "Amplitude : 0"
-                binding.amplitudeTextView.visibility = View.VISIBLE
-                waveRecorder.onAmplitudeListener = {
-                    binding.amplitudeTextView.text = "Amplitude : $it"
-                }
+            if (this::waveRecorder.isInitialized) {
+                if (isChecked) {
+                    binding.amplitudeTextView.text = "Amplitude : 0"
+                    binding.amplitudeTextView.visibility = View.VISIBLE
+                    waveRecorder.onAmplitudeListener = {
+                        binding.amplitudeTextView.text = "Amplitude : $it"
+                    }
 
-            } else {
-                waveRecorder.onAmplitudeListener = null
-                binding.amplitudeTextView.visibility = View.GONE
+                } else {
+                    waveRecorder.onAmplitudeListener = null
+                    binding.amplitudeTextView.visibility = View.GONE
+                }
             }
         }
 
         binding.silenceDetectionSwitch.setOnCheckedChangeListener { _, isChecked ->
-            waveRecorder.silenceDetection = isChecked
-            if (isChecked)
-                Toast.makeText(this, "Noise Suppressor Activated", Toast.LENGTH_SHORT).show()
-
+            if (this::waveRecorder.isInitialized) {
+                waveRecorder.silenceDetection = isChecked
+                if (isChecked)
+                    Toast.makeText(this, "Silence Detection Activated", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.noiseSuppressorSwitch.setOnCheckedChangeListener { _, isChecked ->
-            waveRecorder.noiseSuppressorActive = isChecked
-            if (isChecked)
-                Toast.makeText(this, "Noise Suppressor Activated", Toast.LENGTH_SHORT).show()
+            if (this::waveRecorder.isInitialized) {
+                waveRecorder.noiseSuppressorActive = isChecked
+
+                if (isChecked) {
+                    Toast.makeText(this, "Noise Suppressor Activated", Toast.LENGTH_SHORT).show()
+                }
+            }
 
         }
+    }
+
+    private fun resetSwitches() {
+        binding.showAmplitudeSwitch.isChecked = false
+        binding.silenceDetectionSwitch.isChecked = false
+        binding.noiseSuppressorSwitch.isChecked = false
     }
 
     private fun initRecorder(isSaveToExternalStorage: Boolean) {
@@ -191,9 +238,9 @@ class MainActivity : AppCompatActivity() {
         binding.recordingTextView.visibility = View.GONE
         binding.messageTextView.visibility = View.VISIBLE
         binding.pauseResumeRecordingButton.visibility = View.GONE
-        binding.showAmplitudeSwitch.isChecked = false
         binding.startStopRecordingButton.text = "START"
         binding.noiseSuppressorSwitch.isEnabled = true
+        resetSwitches()
         Toast.makeText(this, "File saved at : $filePath", Toast.LENGTH_LONG).show()
     }
 
@@ -212,6 +259,10 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             waveRecorder.startRecording()
+        }
+        if (requestCode == PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            resetSwitches()
+            initRecorder(isSaveToExternalStorage = true)
         }
     }
 
@@ -235,35 +286,62 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initWithExternalStorage(fileName: String): Boolean {
+    private fun initWithExternalStorage(fileName: String) {
         val folderName = "Android-Wave-Recorder"
         val audioUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
         } else MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-
+        val path =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).absolutePath + "/$folderName/$fileName.wav"
         val contentValues = ContentValues().apply {
             put(MediaStore.Audio.Media.DISPLAY_NAME, "$fileName.wav")
             put(MediaStore.Audio.Media.MIME_TYPE, "audio/x-wav")
-            put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/$folderName")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/$folderName")
+            } else {
+                val file = File(path)
+                val parentFile = file.parentFile
+                if (parentFile != null && !parentFile.exists()) {
+                    parentFile.mkdirs()
+                }
+                put(MediaStore.Audio.AudioColumns.DATA, path)
+            }
         }
-        return try {
-            contentResolver.insert(audioUri, contentValues)?.also { uri ->
-                waveRecorder = WaveRecorder(uri, context = this)
+
+        try {
+            val existingUri = contentResolver.query(
+                audioUri,
+                arrayOf(MediaStore.Audio.Media._ID),
+                "${MediaStore.Audio.AudioColumns.DATA}=?",
+                arrayOf(path),
+                null
+            )?.use { cursor ->
+                if (cursor.moveToFirst()) {
+                    ContentUris.withAppendedId(
+                        audioUri,
+                        cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+                    )
+                } else {
+                    null
+                }
+            }
+
+            val uri = existingUri ?: contentResolver.insert(audioUri, contentValues)
+            uri?.let {
+                waveRecorder = WaveRecorder(it, context = this)
                     .configureWaveSettings {
                         sampleRate = 44100
-                        channels = AudioFormat.CHANNEL_IN_STEREO
-                        audioEncoding = AudioFormat.ENCODING_PCM_32BIT
+                        channels = AudioFormat.CHANNEL_IN_MONO
+                        audioEncoding = AudioFormat.ENCODING_PCM_FLOAT
                     }.configureSilenceDetection {
-                        minAmplitudeThreshold = 80
+                        minAmplitudeThreshold = 2000
                         bufferDurationInMillis = 1500
                         preSilenceDurationInMillis = 1500
                     }
                 filePath = "/Music/$folderName/$fileName.wav"
-            } ?: throw IOException("Couldn't create MediaStore entry")
-            true
-        } catch (e: IOException) {
+            }
+        } catch (e: Exception) {
             e.printStackTrace()
-            false
         }
     }
 
@@ -274,10 +352,10 @@ class MainActivity : AppCompatActivity() {
         waveRecorder = WaveRecorder(filePath)
             .configureWaveSettings {
                 sampleRate = 44100
-                channels = AudioFormat.CHANNEL_IN_STEREO
-                audioEncoding = AudioFormat.ENCODING_PCM_32BIT
+                channels = AudioFormat.CHANNEL_IN_MONO
+                audioEncoding = AudioFormat.ENCODING_PCM_FLOAT
             }.configureSilenceDetection {
-                minAmplitudeThreshold = 80
+                minAmplitudeThreshold = 2000
                 bufferDurationInMillis = 1500
                 preSilenceDurationInMillis = 1500
             }
